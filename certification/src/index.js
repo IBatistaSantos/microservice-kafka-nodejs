ce19457389e98d5ae39cdc523d1cb4a9f2d3d803
Israel dos Santos Perreira Batista <ibatista@informs.conder>
2021-06-07 13:12:54 -0300

    Developed microservice kafka

---
+++ certification/src/index.js
@@ -0,0 +1,41 @@
+import { Kafka, logLevel } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "certificate",
+  brokers: ["localhost:9092"],
+  retry: {
+    initialRetryTime: 300,
+    retries: 10
+  },
+});
+
+const topic = 'issue-certificate';
+const consumer = kafka.consumer({ groupId: "certificate-group" });
+const producer = kafka.producer();
+
+
+async function run () {
+  await producer.connect();
+  await consumer.connect();
+  
+  await consumer.subscribe({ topic });
+  await consumer.run({
+    eachMessage: async ({topic, partition, message}) => {
+      const prefix = `${topic}[${partition}] | ${message.offset}] / ${message.timestamp} `;
+      console.log(`- ${prefix} ${message.key}##${message.value}`);
+     
+      const payload = JSON.parse(message.value);
+
+        producer.send({
+          topic: 'certification-response',
+          messages: [
+            {
+              value: `Certificado do usuário ##${payload.user.name} do curso ${payload.course} gerado!`,
+            }
+          ]
+        });
+    }
+  })
+}
+ 
+run().catch(console.error);
